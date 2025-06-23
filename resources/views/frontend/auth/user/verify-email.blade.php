e659d89f5836ac3923e8b61927574a61e407cf62
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-06-23 12:46:28 +0600

    Frontend design verified card  updated

--- resources/views/frontend/auth/user/verify-email.blade.php
+++ resources/views/frontend/auth/user/verify-email.blade.php
@@ -28,45 +28,46 @@
             </div>
 
 
-            <div
-                class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg relative">
-                <a href="{{ url('/') }}"
-                    class="flex items-center justify-center absolute top-3 left-3  px-5 py-3 rounded-md animate-scalePulse text-gray-700 gap-2">
-                    <i data-lucide="arrow-left"></i>
-                    <span>Back To Home</span>
-                </a>
-                <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-                    {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-                </div>
-                <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-                    {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-                </div>
-
-                @if (session('status') == 'verification-link-sent')
-                    <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-                        {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+            <div class="relative">
+                <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md sm:rounded-lg">
+                    <a href="{{ url('/') }}"
+                        class="flex items-center justify-center absolute -top-7 -left-5  px-5 py-3 rounded-md animate-scalePulse text-gray-700 gap-2">
+                        <i data-lucide="arrow-left"></i>
+                        <span>Back To Home</span>
+                    </a>
+                    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+                        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
                     </div>
-                @endif
+                    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+                        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                    </div>
 
-                <div class="mt-4 flex items-center justify-between">
-                    <form method="POST" action="{{ route('verification.send') }}">
-                        @csrf
-
-                        <div>
-                            <x-primary-button class="py-6">
-                                {{ __('Resend Verification Email') }}
-                            </x-primary-button>
+                    @if (session('status') == 'verification-link-sent')
+                        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+                            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
                         </div>
-                    </form>
+                    @endif
 
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
+                    <div class="mt-4 flex items-center justify-between">
+                        <form method="POST" action="{{ route('verification.send') }}">
+                            @csrf
 
-                        <button type="submit"
-                            class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                            {{ __('Log Out') }}
-                        </button>
-                    </form>
+                            <div>
+                                <x-primary-button class="py-6">
+                                    {{ __('Resend Verification Email') }}
+                                </x-primary-button>
+                            </div>
+                        </form>
+
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <button type="submit"
+                                class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                                {{ __('Log Out') }}
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
